c9ffb3ae41d901eefd365489812d097792e573ca
santiagouzi <santiagoh8525@gmail.com>
2025-03-11 13:32:11 -0500

    almost done

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,35 @@
-# TODO: Add code here
+from typing import List, Dict
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+    def mark_completed(self):
+        self.completed = True
+    def add_tag(self, tag:str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+    def __str__(self):
+        return f"{self.code_id} - {self.title}"
+
+class TodoBook:
+    def __init__(self):
+        self.todos: Dict[int, Todo] = {}
+    def add_todo(self, title: str, description: str) ->int :
+        code_id = len(self.todos) + 1
+        todo = Todo(code_id, title, description)
+        self.todos[code_id] = todo
+        return code_id
+
+    def pending_todos(self)-> List[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
+    def completed_todos(self)-> List[Todo]:
+        pass
+
+
+
+
+
